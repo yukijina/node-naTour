121ea9c8dc4fae9ba348dc3ff44dce171aad34b5
yukijina <yukijina@gmail.com>
2020-02-20 11:57:42 -0800

    create delete method

--- app.js
+++ app.js
@@ -94,6 +94,21 @@ app.patch(`/api/v1/tours/:id`, (req, res) => {
   })
 })
 
+// Delete the data 
+app.delete(`/api/v1/tours/:id`, (req, res) => {
+  if(req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null // the data no longer exsit because we deleted
+  })
+})
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`)
