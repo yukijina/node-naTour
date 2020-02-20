dba12925909bbcebaa4bfa7ea99c8553f024401c
yukijina <yukijina@gmail.com>
2020-02-20 11:52:07 -0800

    create patch method

--- app.js
+++ app.js
@@ -77,6 +77,23 @@ app.post('/api/v1/tours', (req, res) => {
   });
 });
 
+// Update the data - patch (only update the property, put changes entire data)
+app.patch(`/api/v1/tours/:id`, (req, res) => {
+  if(req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    }
+  })
+})
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`)
