7c6cea2be4ea6bbc8a97b05a119c4fe990e429f8
yukijina <yukijina@gmail.com>
2020-02-29 18:06:29 -0800

    sec.99 api alias - more filter and sort

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -16,6 +16,10 @@ const router = express.Router();
 // Add it to the post handler stack
 
 router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router
   .route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour);
