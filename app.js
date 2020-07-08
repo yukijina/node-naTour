38e9b8fc673557d2316846bcf6d24edd12eb9083
yukijina <yukijina@gmail.com>
2020-07-07 19:50:01 -0700

    create global error folder

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -39,24 +41,18 @@ app.all('*', (req, res, next) => {
 
   //reafactor using errorHandler
   // new Error - we can pass a string. that will be a err.message
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
 
   // By adding err in the next(), express knows that it is going to error handler
-  next(err);
+  //next(err);
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 // Express has error handling middleware
 // always add with this order. (err, req, res, next)
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ utils/appError.js
@@ -0,0 +1,16 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message); // extends from parents class
+
+    this.statusCode = statusCode;
+    // startsWith in js takes string. We change statusCode to string and if it includes '4', returns fail. Or error.
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    // this is useful in the future we can send errors to client only if this isOperational is true. We will have
+    // other errors like bugs or programming but those won't be sent to the clients
+    this.isOperational = true;
+    // we can capture err.stack to constructor
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = AppError;
