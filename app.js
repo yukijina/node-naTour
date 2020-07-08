7a85ba47e4a7bbe9c524d03439d4e997554b8ba8
yukijina <yukijina@gmail.com>
2020-07-07 19:17:10 -0700

    create error handling middleware

--- app.js
+++ app.js
@@ -32,9 +32,30 @@ app.use('/api/v1/users', userRouter);
 // Error handler for wrong routes - if the above mounting rounters run to the correct URL, this code never runs.
 // However if ther is an error, this code runs. So it is important to write this code below  mounting routes.
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    staetus: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`
+  // res.status(404).json({
+  //   staetus: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`
+  // });
+
+  //reafactor using errorHandler
+  // new Error - we can pass a string. that will be a err.message
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  // By adding err in the next(), express knows that it is going to error handler
+  next(err);
+});
+
+// Express has error handling middleware
+// always add with this order. (err, req, res, next)
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 
