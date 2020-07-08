160877ec90fc35fe0d2df4af948ca6d204ded9e1
yukijina <yukijina@gmail.com>
2020-07-08 16:52:18 -0700

    add error handling for the wrong route/id

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -35,6 +36,13 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   //console.log(req.query);
   const tour = await Tour.findById(req.params.id);
+
+  // When there is no matched route(tour - same id digits but wrong id number)
+  // we should add this error for the route that includes id
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -62,6 +70,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     new: true, //after update, it returns the documents
     runValidators: true // if you make it false, validation does not work
   });
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -71,7 +84,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
 
   res.status(204).json({
     status: 'success',
