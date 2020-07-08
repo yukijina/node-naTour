1f9748f6492060ec32283299ca2900a076c425d6
yukijina <yukijina@gmail.com>
2020-07-07 19:05:10 -0700

    add error handler for wrong url

--- app.js
+++ app.js
@@ -16,13 +16,8 @@ app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
 
-//// middleware function
-//// argumnets are the ones that you want to add to middleware stack - morgan
+//// middleware
 //// this applied to every single request
-app.use((req, res, next) => {
-  console.log('Hello from the middleware!');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -32,5 +27,15 @@ app.use((req, res, next) => {
 //Mounting router
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// HTTP handles all the routes
+// Error handler for wrong routes - if the above mounting rounters run to the correct URL, this code never runs.
+// However if ther is an error, this code runs. So it is important to write this code below  mounting routes.
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    staetus: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  });
+});
 
 module.exports = app;
