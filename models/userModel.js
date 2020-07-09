03f6f13ca6ddd62a83facfe446c1e5a4b66fab45
yukijina <yukijina@gmail.com>
2020-07-08 20:10:07 -0700

    create user schema

---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email.'],
+    unique: true,
+    lowecase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide apassword'],
+    minlength: 8
+  },
+  passwordConfirme: {
+    type: String,
+    required: [true, 'Please confirm your password']
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
