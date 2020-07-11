ec74ee30f3bf4e8041e284d54bb6812ca7b8fdcc
yukijina <yukijina@gmail.com>
2020-07-11 10:44:29 -0700

    update current user information

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,16 @@ const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
+const filterObj = (obj, ...allowedFields) => {
+  // Object.keys returns array contains keys
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  console.log(newObj);
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
 
@@ -10,6 +20,33 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     status: 'success',
     data: {
       users
+    }
+  });
+});
+
+// change name and password
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password update. {;ease use /updateMyPassword.'
+      )
+    );
+  }
+  // 2) Filtered outt unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user document
+  // We should not use .save here. .save is good for password change
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
     }
   });
 });
