e4ef3f4dfac73f3d9cd44940175b8c316d3ddcd9
yukijina <yukijina@gmail.com>
2021-01-19 13:44:44 -0800

    prevent duplicate review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -90,6 +90,9 @@ reviewSchema.post('save', function() {
   this.constructor.calcAverageRatings(this.tour);
 });
 
+// one user can have only one review
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^findOneAnd/, async function(next) {
   this.r = await this.findOne();
   console.log(this.r);
