091b626e248a2e2da1a2306b6176ab4531f9c5ff
yukijina <yukijina@gmail.com>
2021-02-12 10:53:15 -0800

    add account page and function

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -40,3 +40,9 @@ exports.getLoginForm = (req, res, next) => {
     title: 'Log into your account'
   });
 };
+
+exports.getAccount = (req, res) => {
+  res.status(200).render('account', {
+    title: 'Your account'
+  });
+};
