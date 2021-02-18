abe1e5508da9cee9a6305a2a9cab97ee25dc4f69
yukijina <yukijina@gmail.com>
2021-02-18 15:36:44 -0800

    add default pic and filename

--- controllers/userController.js
+++ controllers/userController.js
@@ -63,6 +63,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
   // 2) Filtered out unwanted fields names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if(req.file) filteredBody.photo = req.file.filename;
 
   // 3) Update user document
   // We should not use .save here. .save is good for password change
