de7024ddbb9b2e89b2d1f5379f750912636b4f3f
yukijina <yukijina@gmail.com>
2021-02-19 14:24:52 -0800

    resize image - sharp

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
@@ -11,30 +12,47 @@ exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
 
 // store file in file system - check multer documentation for multer
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {,
-    cb(null, 'publc/img/users');
-  },
-  filename: (req, file, cb) => {
-    // user-xxxx-getaughu.jpeg 
-    const ext = file.mimetype.split('/')[1];
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`)
- }});
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {,
+//     cb(null, 'publc/img/users');
+//   },
+//   filename: (req, file, cb) => {
+//     // user-xxxx-getaughu.jpeg
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`)
+//  }});
+
+// image is stored as buffer
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
-    cb(null, true)
+    cb(null, true);
   } else {
-    cb(new AppError('Not an image! Please upload only image', 400), false)
+    cb(new AppError('Not an image! Please upload only image', 400), false);
   }
 };
 
-const upload = multer({ 
+const upload = multer({
   storage: multerStorage,
   fileFilter: multerFilter
- });
+});
 
 exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+  //image size 500 x 500, quality 90%
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+};
 
 const filterObj = (obj, ...allowedFields) => {
   // Object.keys returns array contains keys
@@ -63,7 +81,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
   // 2) Filtered out unwanted fields names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
-  if(req.file) filteredBody.photo = req.file.filename;
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 3) Update user document
   // We should not use .save here. .save is good for password change
