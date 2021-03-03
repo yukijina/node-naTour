0ceaf07248bf57ddd5c5526a35fac7e213e1844f
yukijina <yukijina@gmail.com>
2021-03-03 14:58:12 -0800

    stripe frontend

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,7 @@ import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
+import { bookTour } from './stripe';
 
 // DOM Element
 const mapBox = document.getElementById('map');
@@ -9,6 +10,7 @@ const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
 
 // Delegation
 if (mapBox) {
@@ -58,4 +60,11 @@ if (userPasswordForm)
     document.getElementById('password-current').value = '';
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
+  });
+
+if (bookBtn)
+  bookBtn.addEventListener('click', e => {
+    e.target.textContent = 'Processiong...';
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
   });
---
+++ public/js/stripe.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { showAler } from './alerts';
+const stripe = Stripe('pk_test_BUxxxxx-xxxxxxxxxxxxxx');
+
+export const bookTour = async tourId => {
+  try {
+    // 1)  Get checkout session from API
+    const session = await axios(
+      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log('session==>', session);
+
+    // 2) Create checkout for + share credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
