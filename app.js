31e8f1aefadf5d3efad385614bb17ec4252695f3
yukijina <yukijina@gmail.com>
2021-03-28 11:11:02 -0700

    add cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -22,9 +23,16 @@ const app = express();
 app.enable('trust proxy');
 
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
 
 //// GLOBAL MIDDLEWARE
+// Imprement cors - enable Access-Controll-Allow-Origin *
+app.use(cors());
+// allows non-simple requests ex.patch, delete (preflight)
+app.options('*', cors());
+
+// Serving static files
+app.set('views', path.join(__dirname, 'views'));
+
 // SET SECURITY HEADERS - extra hedder keys are added for security
 app.use(helmet());
 
